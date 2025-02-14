f05a8b30c518b1e419e0b5b25d6c7eb974979665
mirupio <162397816+mirupio@users.noreply.github.com>
2025-02-14 16:37:26 +0900

    chore: repository 패키지 생성 및 JPA repository 작성

---
+++ src/main/java/com/untitled/cherrymap/repository/RouteRepository.java
@@ -0,0 +1,7 @@
+package com.untitled.cherrymap.repository;
+
+import com.untitled.cherrymap.domain.Route;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RouteRepository extends JpaRepository<Route, Long> {
+}
