a9f03b0a7e3e08e85f09935e52def50e25474470
astrokan <astrokan@naver.com>
2025-02-17 03:15:04 +0900

    fix: 유저 정보 조회 메서드 수정

--- src/main/java/com/untitled/cherrymap/controller/MemberController.java
+++ src/main/java/com/untitled/cherrymap/controller/MemberController.java
@@ -1,40 +1,22 @@
 package com.untitled.cherrymap.controller;
 
 import com.untitled.cherrymap.domain.Member;
-import com.untitled.cherrymap.repository.MemberRepository;
+import com.untitled.cherrymap.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
+@RequiredArgsConstructor
 public class MemberController {
 
-    private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
-    public MemberController(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    @GetMapping("/{memberId}/info")
-    public ResponseEntity<?> getCurrentUser(@AuthenticationPrincipal OAuth2User user) {
-        if (user == null) {
-            return ResponseEntity.badRequest().body("User not authenticated");
-        }
-
-        String providerId = user.getAttribute("id");
-        Member member = memberRepository.findByProviderId(providerId);
-
-        if (member == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.ok(Map.of(
-                "nickname", member.getNickname(),
-                "email", member.getEmail()
-        ));
+    @GetMapping("/api/{providerId}/info")
+    public ResponseEntity<Member> getMember(@PathVariable String providerId) {
+        Member member = memberService.getMemberByProviderId(providerId);
+        return ResponseEntity.ok().body(member);
     }
 }
