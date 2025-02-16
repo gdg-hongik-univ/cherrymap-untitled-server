2055ef358c962f9ce122d6bc86fc9c5d4f4d3e4c
mirupio <162397816+mirupio@users.noreply.github.com>
2025-02-16 18:51:20 +0900

    feat: 알림 메시지 템플릿 불러오기 기능 TemplateService 구현

--- src/main/java/com/untitled/cherrymap/service/TemplateService.java
+++ src/main/java/com/untitled/cherrymap/service/TemplateService.java
@@ -11,14 +11,14 @@ public class TemplateService {
         Map<String, String> template = new HashMap<>();
 
         if ("subway".equalsIgnoreCase(mode)) {
-            template.put("beforeBoarding", "다음 역이 '{nextStation}'인지 확인하세요.");
-            template.put("beforeExit", "다음 역에서 내릴 준비하세요!!");
-            template.put("onExit", "{endStation}역입니다! 내리세요!");
+            template.put("beforeBoarding", "다음 역이 '{nextStation}'인지 확인하고 탑승하세요.");
+            template.put("beforeExit", "다음 역인 '{endStation}'에서 하차하셔야 합니다. 내릴 준비하세요!!");
+            template.put("onExit", "'{endStation}'도착입니다! 지금 당장 내리세요!");
         }
         else if ("bus".equalsIgnoreCase(mode)) {
-            template.put("beforeBoarding", "버스 기사님께 '{endStation}'로 가는 버스인지 여쭤보세요.");
-            template.put("beforeExit", "다음 정류장에서 내릴 준비하세요!!");
-            template.put("onExit", "{endStation} 도착입니다! 내리세요!");
+            template.put("beforeBoarding", "버스 기사님께 '{nextStation}'쪽으로 가는 버스인지 여쭤보고 탑승하세요.");
+            template.put("beforeExit", "다음 정류장인 '{endStation}'에서 하차하셔야 합니다. 내릴 준비하세요!!");
+            template.put("onExit", "'{endStation}' 도착입니다! 지금 당장 내리세요!");
         }
 
         return template;
