e68105ae771a54a87023a6eab7753bd9b439a763
mirupio <162397816+mirupio@users.noreply.github.com>
2025-02-17 02:29:25 +0900

    chore: AlertMessageController에 swagger 관련 annotation 추가

--- src/main/java/com/untitled/cherrymap/controller/AlertMessageController.java
+++ src/main/java/com/untitled/cherrymap/controller/AlertMessageController.java
@@ -2,11 +2,15 @@ package com.untitled.cherrymap.controller;
 
 import com.untitled.cherrymap.domain.RouteRequest;
 import com.untitled.cherrymap.service.AlertMessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name="AlertMessage-Controller",description = "대중교통 별 알림 메시지 반환 API")
 @RestController
 public class AlertMessageController {
 
@@ -16,7 +20,8 @@ public class AlertMessageController {
         this.alertMessageService = alertMessageService;
     }
 
-    @GetMapping("/api/status")
+    @Operation(summary = "현재 상태 반영하여 경로 요청", description = "주어진 경로 요청(RouteRequest) 정보를 기반으로 알림 메시지를 반환합니다.")
+    @GetMapping("/status")
     public ResponseEntity<String> getStatus(@RequestBody RouteRequest routeRequest) {
 
         return ResponseEntity.ok(alertMessageService.getAlertMessage(
