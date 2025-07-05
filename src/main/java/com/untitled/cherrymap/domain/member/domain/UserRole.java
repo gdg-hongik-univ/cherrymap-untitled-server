a473b4a85f7a0628384a6a95cb150737fd68b2b7
astrokan <astrokan@naver.com>
2025-07-05 13:46:47 +0900

    fix: enum 필드 변경 작업

---
+++ src/main/java/com/untitled/cherrymap/domain/member/domain/UserRole.java
@@ -0,0 +1,7 @@
+package com.untitled.cherrymap.domain.member.domain;
+
+public enum UserRole {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_MODERATOR
+} 
---
+++ src/main/java/com/untitled/cherrymap/domain/route/domain/RouteMode.java
@@ -0,0 +1,6 @@
+package com.untitled.cherrymap.domain.route.domain;
+
+public enum RouteMode {
+    도보,
+    대중교통
+} 
