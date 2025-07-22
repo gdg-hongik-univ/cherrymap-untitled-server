bb6ee8d1851a6b94e68987038d449edfd06938df
astrokan <astrokan@naver.com>
2025-07-23 05:31:34 +0900

    chore: swagger-ui 사용 시 dev/prod server dropdown 분리

--- src/main/java/com/untitled/cherrymap/common/config/OpenApiConfig.java
+++ src/main/java/com/untitled/cherrymap/common/config/OpenApiConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -31,6 +34,10 @@ public class OpenApiConfig {
 
         return new OpenAPI()
                 .info(info)
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local Development"),
+                        new Server().url("https://cherrymap.click").description("Production Server")
+                ))
                 .addSecurityItem(securityRequirement)
                 .components(new Components().addSecuritySchemes("Authorization", securityScheme));
     }
